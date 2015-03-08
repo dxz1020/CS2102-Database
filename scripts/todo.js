c5de55ac4e8074b9afbdb7604fc1c07644f11bfb
Duan Xu Zhou <dxz1020@hotmail.com>
2015-03-09 01:24:18 +0800

    login feature stub

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,17 +1,16 @@
 angular.module('store',['ionic'])
 
 .controller('storeCtrl', function($scope, $ionicModal, $ionicPopup){
-	$ionicModal.fromTemplateUrl('new-task.html', function(modal){
+	$ionicModal.fromTemplateUrl('login.html', function(modal){
 		$scope.taskModal = modal;
 	}, {
 		scope: $scope,
 		animation: 'slide-in-up'
 	});
-	
-	$ionicModal.fromTemplateUrl('new-task_1.html', function(modal) {
-    $scope.taskModal_1 = modal;
-  	}, {
-    scope: $scope,
-    animation: 'slide-in-up'
-  	});
+	$scope.login = function(){
+		$scope.taskModal.show();
+	};
+	$scope.close = function(){
+		$scope.taskModal.hide();
+	}
 });
