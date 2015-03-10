bf3b7478d6e95ba212557016678675888b61e84b
zixian92 <zixian1992@hotmail.com>
2015-03-10 23:54:06 +0800

    Fixes dbsetup.sql to run on SQLDeveloper

--- dbsetup.sql
+++ dbsetup.sql
@@ -5,81 +5,54 @@ category VARCHAR(64) NOT NULL,
 genre VARCHAR(64) NOT NULL,
 device VARCHAR(64) NOT NULL,
 release_date DATE NOT NULL,
-price DOUBLE(10, 2) NOT NULL CHECK(price>=0),
-rent_price DOUBLE(10, 2) NOT NULL CHECK(price>=0),
-rating INT NOT NULL DEFAULT 0
+price NUMBER(*, 2) NOT NULL CHECK(price>=0),
+rent_price NUMBER(*, 2) NOT NULL CHECK(rent_price>=0),
+rating INT DEFAULT 0 NOT NULL
 );
 
--- delimiter //
--- CREATE TRIGGER item_price_check BEFORE INSERT ON Item
--- FOR EACH ROW
---   BEGIN
---     IF NEW.price<0 THEN
---       SET NEW.price=0;
---     END IF;
---     IF NEW.rent_price<0 THEN
---       SET NEW.rent_price=0;
---     END IF;
---   END;//
--- delimiter ;
-
-CREATE TABLE User (
+CREATE TABLE Accounts (
 email VARCHAR(64) PRIMARY KEY,
 username VARCHAR(64) NOT NULL,
 password VARCHAR(64) NOT NULL,
 admin CHAR(1) NOT NULL CHECK(admin='Y' OR admin='N')
 );
 
--- delimiter //
--- CREATE TRIGGER user_adm_check BEFORE INSERT on User
--- FOR EACH ROW
---  BEGIN
---    IF NEW.admin!='Y' AND NEW.admin!='N' THEN
---      SET NEW.admin='Y';
---    END IF;
---  END;//
--- delimiter ;
-
 CREATE TABLE Purchase (
-user VARCHAR(64) REFERENCES User(email),
+customer VARCHAR(64) REFERENCES Accounts(email),
 item VARCHAR(32) REFERENCES Item(item_id),
 purchase_date DATE NOT NULL,
-PRIMARY KEY (user, item)
+PRIMARY KEY (customer, item)
 );
 
 CREATE TABLE Rent (
-user VARCHAR(64) REFERENCES User(email),
+customer VARCHAR(64) REFERENCES Accounts(email),
 item VARCHAR(32) REFERENCES Item(item_id),
 borrow_date DATE NOT NULL,
-due_date DATE NOT NULL CHECK(borrow_date<=due_date),
-return_date DATE CHECK(return_date IS NULL OR borrow_date<=return_date),
-PRIMARY KEY(user, item, borrow_date)
+due_date DATE NOT NULL,
+return_date DATE,
+PRIMARY KEY(customer, item, borrow_date),
+CHECK(borrow_date<=due_date),
+CHECK(return_date IS NULL OR borrow_date<=return_date)
 );
 
--- How to enforce date checks using trigger?
--- Eg. prevent insertion with due dat eearlier than borrow date.
-
 CREATE TABLE Rating (
-user VARCHAR(64) REFERENCES User(email),
+customer VARCHAR(64) REFERENCES Accounts(email),
 item VARCHAR(32) REFERENCES Item(item_id),
 rating INT NOT NULL CHECK(rating>0 AND rating<=5),
-PRIMARY KEY (user, item)
+PRIMARY KEY (customer, item)
 );
 
 -- Trigger to update item's overall rating after inserting a new rating
-delimiter //
 CREATE TRIGGER update_item_rating AFTER INSERT ON Rating
-FOR EACH ROW
 BEGIN
-UPDATE Item SET rating = (SELECT AVG(rating) FROM Rating GROUP BY item HAVING Item.item_id=item) WHERE item_id=NEW.item;
-END;//
-delimiter ;
+UPDATE Item SET rating = (SELECT AVG(rating) FROM Rating GROUP BY item HAVING Item.item_id=item) WHERE EXISTS (SELECT * FROM Rating WHERE item=Item.item_id);
+END;
+/
 
 -- Inserting  a few dummy values to test integrity constraints and triggers.
-INSERT INTO Item VALUES ('1', 'Brave Frontier', 'Game', 'RPG', 'Android', '2013-1-1', -1, -1, 0); -- invalid query
-INSERT INTO Item VALUES ('1', 'Brave Frontier', 'Game', 'RPG', 'Android', '2013-1-1', 0, 0, 0);
-INSERT INTO Item VALUES ('2', 'The Hobbit: An Unexpected Journey', 'Movie', 'Fantasy', 'DVD', '2012-12-15', 30, 13, 0);
-INSERT INTO User VALUES('zx@email.com', 'zixian', 'password', 'A');
-INSERT INTO Purchase VALUES ('zx@email.com', '1', '2014-4-10');
-INSERT INTO Rent VALUES ('zx@email.com', '2', '2014-12-15', '2014-12-29', NULL);
+INSERT INTO Item VALUES ('1', 'Brave Frontier', 'Game', 'RPG', 'Android', to_date('2013-1-1', 'yyyy-mm-dd'), 0, 0, 0);
+INSERT INTO Item VALUES ('2', 'The Hobbit: An Unexpected Journey', 'Movie', 'Fantasy', 'DVD', to_date('2012-12-15', 'yyyy-mm-dd'), 30, 13, 0);
+INSERT INTO Accounts VALUES('zx@email.com', 'zixian', 'password', 'Y');
+INSERT INTO Purchase VALUES ('zx@email.com', '1', to_date('2014-4-10', 'yyyy-mm-dd'));
+INSERT INTO Rent VALUES ('zx@email.com', '2', to_date('2014-12-15', 'yyyy-mm-dd'), to_date('2014-12-29', 'yyyy-mm-dd'), NULL);
 INSERT INTO Rating VALUES ('zx@email.com', '1', 4);
