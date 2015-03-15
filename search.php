fae0f6b0fd73dfd854693dd90df473811b47c9f4
zixian92 <zixian1992@hotmail.com>
2015-03-15 21:54:13 +0800

    Adds code for connecting to database in search.php

--- search.php
+++ search.php
@@ -2,5 +2,20 @@
   /* search.php
    * This script handles request to search for digital store content.
    */
+
+  putenv("ORACLE_HOME=/oraclient");
+  $dbuser = "a0110781";
+  $dbpassword = "Nana7Nana";
+  $dbinfo = "(DESCRIPTION = (ADDRESS_LIST = (
+      ADDRESS = (PROTOCOL = TCP)(HOST = sid3.comp.nus.edu.sg)(PORT = 1521))
+    )(CONNECT_DATA = (SERVICE_NAME = sid3.comp.nus.edu.sg)))";
+
+  $dbh = oci_connect($dbuser, $dbpassword, $dbinfo);
+  if(!$dbh){
+    http_response_code(503);
+    exit(1);
+  }
+
+  oci_close($dbh);
   echo $_GET['message'];
 ?>
