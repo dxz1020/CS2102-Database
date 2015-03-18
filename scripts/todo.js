4a5a5cb90e7e44aa4ef4f94638ba514bb4658019
sharmine <slxmlow@gmail.com>
2015-03-18 22:10:52 +0800

    added javascript functions

--- scripts/todo.js
+++ scripts/todo.js
@@ -16,7 +16,71 @@ angular.module('store',['ionic'])
 });
 
 function ContentController($scope, $ionicSideMenuDelegate) {
-  $scope.toggleLeft = function() {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
+	$scope.toggleLeft = function() {
+		$ionicSideMenuDelegate.toggleLeft();
+	};
+
+	$scope.getApp = function() {
+		$.ajax({
+			method: "GET",
+			url: "search.php",
+			data: { category: 'App'
+			}
+		})
+		.done(function(msg) {
+			var listOfApps = JSON.parse(msg);
+			checkArr(listOfApps);       
+		})
+		.fail(function(msg) {
+			console.log("failed"); 
+		});
+	};
+
+	$scope.getMovie = function() {
+		$.ajax({
+			method: "GET",
+			url: "search.php",
+			data: { category: "Movie"
+			}
+		})
+		.done(function(msg) {
+			var listOfMovie = JSON.parse(msg);
+			checkArr(listOfMovie);       
+		})
+		.fail(function(msg) {
+			console.log("failed"); 
+		});
+	};
+
+	$scope.getTV = function() {
+		$.ajax({
+			method: "GET",
+			url: "search.php",
+			data: { category: "TV"
+		}
+		})
+		.done(function(msg) {
+			var listOfTV = JSON.parse(msg);
+			checkArr(listOfTV);       
+		})
+		.fail(function(msg) {
+			console.log("failed"); 
+		});
+	};
+
+	$scope.getGame = function() {
+		$.ajax({
+			method: "GET",
+			url: "search.php",
+			data: { category: "Game"
+			}
+		})
+		.done(function(msg) {
+			var listOfGame = JSON.parse(msg);
+			checkArr(listOfGame);       
+		})
+		.fail(function(msg) {
+			console.log("failed "); 
+		});
+	};
 }
