8d3161776c9e5501b520a93095e72d4da35ba8aa
zixian92 <zixian1992@hotmail.com>
2015-04-05 23:55:22 +0800

    Fixes bug in logout.php

--- logout.php
+++ logout.php
@@ -3,12 +3,11 @@
    * Handles logout requests.
    */
 
-  if(session_status()==PHP_SESSION_ACTIVE){
-    echo "Logging out...";
-    unset($_SESSION['email']);
-    unset($_SESSION['username']);
-    unset($_SESSION['admin']);
-    session_destroy();
-  }
+  session_start();
+  unset($_SESSION['email']);
+  unset($_SESSION['username']);
+  unset($_SESSION['admin']);
+  session_destroy();
+  
   header("Location: http://cs2102-i.comp.nus.edu.sg/~a0110781/");
 ?>
