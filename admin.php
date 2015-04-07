7081fbef118b3e3f763d71eaa7e771b426feea33
Zhi Hua <ngzhihua_@hotmail.com>
2015-04-07 20:32:48 +0800

    Implemented delete item and delete accounts, not tested.

--- admin.php
+++ admin.php
@@ -4,8 +4,8 @@
    */
 
   putenv("ORACLE_HOME=/oraclient");
-  $dbuser = "a0111217";		//Change to your own account
-  $dbpassword = "crse1420";	//Change to appropriate password
+  $dbuser = "a0110781";		//Change to your own account
+  $dbpassword = "Nana7Nana";	//Change to appropriate password
   $dbinfo = "(DESCRIPTION = (ADDRESS_LIST = (
       ADDRESS = (PROTOCOL = TCP)(HOST = sid3.comp.nus.edu.sg)(PORT = 1521))
     )(CONNECT_DATA = (SERVICE_NAME = sid3.comp.nus.edu.sg)))";
@@ -67,7 +67,7 @@
       http_response_code(400);
       return;
     }
-
+ 
     $params = json_decode(file_get_contents('php://input'), true);
     if(!isset($params['itemid'])){
       http_response_code(400);
@@ -77,30 +77,38 @@
     $dbh = connectToDatabase();
 
     // Actual deletion
-    $query1 = "DELETE FROM Purchase WHERE item = (:itemid)";
-    $stmt1 = oci_parse($dbh, $query1);
-    oci_bind_by_name($stmt1, ":itemid", $params['itemid']);
-    oci_free_statement($stmt1);
-
-    $query2 = "DELETE FROM Likes WHERE item = (:itemid)";
-    $stmt2 = oci_parse($dbh, $query2);
-    oci_bind_by_name($stmt2, ":itemid", $params['itemid']);
-    oci_free_statement($stmt2);
-
-     $query3 = "DELETE FROM Rent WHERE item = (:itemid)";
-    $stmt = oci_parse($dbh, $query3);
-    oci_bind_by_name($stmt3, ":itemid", $params['itemid']);
-    oci_free_statement($stmt3);
-
-    $query4 = "DELETE FROM Item WHERE item_id = (:itemid)";
-    $stmt = oci_parse($dbh, $query4);
-    oci_bind_by_name($stmt4, ":itemid", $params['itemid']);
-    oci_execute($stmt4);
-    oci_free_statement($stmt4);
+    $query = "DELETE FROM Item WHERE item_id = (:itemid)";
+    $stmt = oci_parse($dbh, $query);
+    oci_bind_by_name($stmt, ":itemid", $params['itemid']);
+    oci_free_statement($stmt);
 
     closeConnection($dbh);
 
   }
+
+  function deleteAccount(){
+    if($_SERVER['REQUEST_METHOD']!='POST' ||
+  $_SERVER['CONTENT_TYPE']!='application/json'){
+      http_response_code(400);
+      return;
+    }
+
+    $params = json_decode(file_get_contents('php://input'), true);
+    if(!isset($params['email'])){
+      http_response_code(400);
+      return;
+    }
+
+        // Actual deletion
+    $query = "DELETE FROM Accounts WHERE email = (:email)";
+    $stmt = oci_parse($dbh, $query);
+    oci_bind_by_name($stmt, ":email", $params['email']);
+    oci_free_statement($stmt);
+
+    closeConnection($dbh);
+
+  } 
+
 
   session_start();
 
@@ -114,6 +122,7 @@
 
   switch($_GET['type']){
     case 'add': addItem(); break;
+    case 'deleteAccount': deleteAccount();break;
     case 'deleteItem': deleteItem(); break;
     default: http_response_code(400); break;
   }
